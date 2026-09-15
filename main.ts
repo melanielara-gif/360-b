5235f8bffc25f520d25a16ea1417be11a1a41a96
melanielara-gif <melanie.lara@colsubsidio.edu.co>
2026-09-15 11:32:45 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,98 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # # .
+        # . . # .
+        . . # . .
+        # . . # .
+        . # # # .
+        `)
+    basic.showLeds(`
+        # # # . .
+        . . # . #
+        . # . . #
+        . . # . #
+        # # # . .
+        `)
+    basic.showLeds(`
+        # # . . #
+        . # . # .
+        # . . # #
+        . # . # .
+        # # . . #
+        `)
+    basic.showLeds(`
+        . . . # #
+        . . # . .
+        . . # # #
+        . . # . .
+        . . . # #
+        `)
+    basic.showLeds(`
+        . . # # #
+        . # . . .
+        . # # # #
+        . # . . #
+        . . # # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # # # # #
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        # # . . .
+        . . . . .
+        # # # # .
+        . . . # .
+        # # # . .
+        `)
+    basic.showLeds(`
+        # # . . .
+        . . . . #
+        # # # . #
+        . . # . #
+        # # . . .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . # . # .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . # #
+        # . # . .
+        # . # . .
+        # . # . .
+        . . . # #
+        `)
+    basic.showLeds(`
+        . . # # #
+        . # . . .
+        . # . . .
+        . # . . .
+        . . # # #
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+})
+basic.forever(function () {
+	
+})
